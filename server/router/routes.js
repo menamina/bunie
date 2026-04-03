45bf82214b827f43806f724faee3adac1db8eb18
Mena <dayjacs03@gmail.com>
2026-04-03 18:06:50 -0500

    working on routes

---
+++ server/router/routes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const remote = require("../remote/control");
+const validator = require("../utils/validator");
+const passport = require("../utils/passport");
