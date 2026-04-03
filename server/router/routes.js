095e8360bff6d5cb8b919f6d22d71e41b271b926
Mena <dayjacs03@gmail.com>
2026-04-03 18:58:11 -0500

    routes

--- server/router/routes.js
+++ server/router/routes.js
@@ -39,9 +39,40 @@ router.get("/main-feed-API", isAuth, remote.getMainFeed);
 // getting user's profile //
 router.get("/my-profile-API", isAuth, remote.getProfile);
 router.get("/get-my-profile-settings", isAuth, remote.getMyProfileSettings);
+
 router.get("/get-my-followers", isAuth, remote.getFollowers);
 router.get("/get-my-followers", isAuth, remote.getFollowing);
-router.get("/get-user-posts", isAuth, remote.getUserPosts);
+
+router.get("/get-my-posts", isAuth, remote.getUserPosts);
+
+router.get("/get-my-inventory", isAuth, remote.getUserInventory);
+router.get("/get-my-in-progress", isAuth, remote.getUserInProgress);
+router.get("/get-my-limbo", isAuth, remote.getMy);
+router.get("/get-my-decluttered", isAuth, remote.getUserDecluttered);
+router.get("/get-my-finished", isAuth, remote.getUserFinished);
+router.get("/get-my-likes", isAuth, remote.getUserLikes);
+
+// editing user profile //
+remote.patch("/update-my-profile-API", isAuth, remote.updateUserProfile);
+remote.delete("/delete-my-account-API", isAuth, remote.deleteUserAccount);
+
+// user options //
+
+remote.post("/add-to-inventory-API", isAuth, remote.addProduct);
+// ^^ is it just a regular add to inven, WIP, or limbo //
+remote.post("/add-to-finished-API", isAuth, remote.addFinishedProduct);
+remote.post("/add-to-decluttered-API", isAuth, remote.addDeclutteredProduct);
+
+// user posts + comments //
+
+remote.post("/make-post-API", isAuth, remote.makeAPost);
+remote.post("/make-comment-API", isAuth, remote.makeAComment);
+
+remote.patch("/update-post/:postToUpdate", isAuth, remote.updatePost);
+remote.patch("/update-comment/:commentToUpdate", isAuth, remote.updateComment);
+
+remote.delete("/delete-post/:postToDelete", isAuth, remote.deletePost);
+remote.delete("/delete-comment/:commentToDelete", isAuth, remote.deleteComment);
 
 // getting other ppl's profiles //
-router.get("/my-profile/:id", isAuth, remote.getProfile);
+router.get("/profile-API/:id", isAuth, remote.getProfile);
