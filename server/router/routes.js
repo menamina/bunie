f24101a29b77d2c6948e8f60db205e292763e10b
Mena <dayjacs03@gmail.com>
2026-04-03 19:07:26 -0500

    routes

--- server/router/routes.js
+++ server/router/routes.js
@@ -37,20 +37,20 @@ router.post("login-API", (req, res, next) => {
 router.get("/main-feed-API", isAuth, remote.getMainFeed);
 
 // getting user's profile //
-router.get("/my-profile-API", isAuth, remote.getProfile);
+router.get("/my-profile-API/:id", isAuth, remote.getProfile);
 router.get("/get-my-profile-settings", isAuth, remote.getMyProfileSettings);
 
-router.get("/get-my-followers", isAuth, remote.getFollowers);
-router.get("/get-my-followers", isAuth, remote.getFollowing);
+router.get("/get-my-followers/:id", isAuth, remote.getFollowers);
+router.get("/get-my-followers/:id", isAuth, remote.getFollowing);
 
-router.get("/get-my-posts", isAuth, remote.getUserPosts);
+router.get("/get-my-posts/:id", isAuth, remote.getUserPosts);
 
-router.get("/get-my-inventory", isAuth, remote.getUserInventory);
-router.get("/get-my-in-progress", isAuth, remote.getUserInProgress);
-router.get("/get-my-limbo", isAuth, remote.getMy);
-router.get("/get-my-decluttered", isAuth, remote.getUserDecluttered);
-router.get("/get-my-finished", isAuth, remote.getUserFinished);
-router.get("/get-my-likes", isAuth, remote.getUserLikes);
+router.get("/get-my-inventory/:id", isAuth, remote.getUserInventory);
+router.get("/get-my-in-progress/:id", isAuth, remote.getUserInProgress);
+router.get("/get-my-limbo/:id", isAuth, remote.getUserLimbo);
+router.get("/get-my-decluttered/:id", isAuth, remote.getUserDecluttered);
+router.get("/get-my-finished/:id", isAuth, remote.getUserFinished);
+router.get("/get-my-likes/:id", isAuth, remote.getUserLikes);
 
 // editing user profile //
 remote.patch("/update-my-profile-API", isAuth, remote.updateUserProfile);
@@ -63,7 +63,19 @@ remote.post("/add-to-inventory-API", isAuth, remote.addProduct);
 remote.post("/add-to-finished-API", isAuth, remote.addFinishedProduct);
 remote.post("/add-to-decluttered-API", isAuth, remote.addDeclutteredProduct);
 
-// user posts + comments //
+remote.patch(
+  "/update-inventory-status/:productID",
+  isAuth,
+  remote.updateInventoryStatus,
+);
+
+remote.delete(
+  "/delete-from-where/:productID",
+  isAuth,
+  remote.deleteProductFromHere,
+);
+
+// user posts + comments + likes //
 
 remote.post("/make-post-API", isAuth, remote.makeAPost);
 remote.post("/make-comment-API", isAuth, remote.makeAComment);
@@ -74,5 +86,20 @@ remote.patch("/update-comment/:commentToUpdate", isAuth, remote.updateComment);
 remote.delete("/delete-post/:postToDelete", isAuth, remote.deletePost);
 remote.delete("/delete-comment/:commentToDelete", isAuth, remote.deleteComment);
 
+remote.post("/like-post/:postID", isAuth, remote.togglePostLike);
+remote.post("/like-post/:postID", isAuth, remote.toggleCommentLike);
+
 // getting other ppl's profiles //
 router.get("/profile-API/:id", isAuth, remote.getProfile);
+
+router.get("/get-user-followers/:id", isAuth, remote.getFollowers);
+router.get("/get-user-followers/:id", isAuth, remote.getFollowing);
+
+router.get("/get-user-posts/:id", isAuth, remote.getUserPosts);
+
+router.get("/get-user-inventory/:id", isAuth, remote.getUserInventory);
+router.get("/get-user-in-progress/:id", isAuth, remote.getUserInProgress);
+router.get("/get-user-limbo/:id", isAuth, remote.getUserLimbo);
+router.get("/get-user-decluttered/:id", isAuth, remote.getUserDecluttered);
+router.get("/get-user-finished/:id", isAuth, remote.getUserFinished);
+router.get("/get-user-likes/:id", isAuth, remote.getUserLikes);
