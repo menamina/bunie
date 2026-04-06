96d99b9e14637a60aebf4bac99116aa2ea9a4568
Mena <dayjacs03@gmail.com>
2026-04-06 17:50:28 -0500

    setting up bare settings

---
+++ front-end/src/components/settings.jsx
@@ -0,0 +1,15 @@
+function Settings() {
+  const [settingView, setSettingView] = useState("overview");
+
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Settings;
