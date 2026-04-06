f1e102344f94865474746a5e2d12c63f5b4d0525
Mena <dayjacs03@gmail.com>
2026-04-06 16:49:14 -0500

    barebones template for profile

--- front-end/src/components/profile.jsx
+++ front-end/src/components/profile.jsx
@@ -9,19 +9,46 @@ function Profile() {
       <div>
         {/* div below willstretch view heighgt view width for header */}
         <div className="headerDIV">
-          <div classname="pfpDIV">
-            <img className="pfp" src="" alt="header for.."></img>
+          <div classname="headerDIV">
+            <img className="header" src="" alt="header for.."></img>
           </div>
           <div className="whiteSpace"></div>
         </div>
         {/* div below will be a position absolute and cover header a bit and also be not fulll vh vw */}
-        <div className="contentsDIV">
+        <div className="userINFO">
+            <div>
+                    <img src="" alt="pfp for.."></img>
+            </div>
           <div>
-            <img src="" alt="pfp for.."></img>
+            <div>
+                <div className="">
+                    <div>name</div>
+                    <div>username</div>
+                </div>
+                <div>
+                    <div>bio</div>
+                </div>
+            </div>
+            <div>
+                {/* follow / unfollow */}
+                {/* edit prof if u */}
+            </div>
           </div>
-          <div>
-            <div></div>
-            <div className="renderViewHere">
+        <div>
+        <div>
+        <div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+        </div>
+        </div>
+        <div className="renderViewHere">
                 {view === "overview" &&}
                 {view === "inventory" &&}
                 {view === "inprogress" &&}
@@ -33,7 +60,7 @@ function Profile() {
                 {view === "following" &&}
                 
             </div>
-          </div>
+        </div>
         </div>
       </div>
     </div>
