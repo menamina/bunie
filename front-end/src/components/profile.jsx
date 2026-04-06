f8366c2864b07a8b7280acdb4956a0f1193bdc81
Mena <dayjacs03@gmail.com>
2026-04-06 16:33:20 -0500

    barebones template for profile

---
+++ front-end/src/components/profile.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function Profile() {
+  const [view, setView] = useState("overview");
+
+  return <div className="profileDIV"></div>;
+}
+
+export default Profile;
