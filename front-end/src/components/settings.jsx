c472870c189ce0418553d818c860db00557dca1b
Mena <dayjacs03@gmail.com>
2026-04-06 17:53:24 -0500

    setting up bare settings

--- front-end/src/components/settings.jsx
+++ front-end/src/components/settings.jsx
@@ -1,8 +1,19 @@
 function Settings() {
   const [settingView, setSettingView] = useState("overview");
 
+  const [imgChange, setIMGChange] = useState({
+    pfp: "",
+    header: "",
+  });
+
+  const [changePassword, setChangePassword] = useState({
+    oldPassword: "",
+    newPassword: "",
+    confirmNewpassword: "",
+  });
+
   return (
-    <div>
+    <div className="settingsDIV">
       <div></div>
       <div></div>
       <div></div>
