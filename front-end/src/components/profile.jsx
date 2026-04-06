4fdad9930156aac45a26bc8e600504b13c03d314
Mena <dayjacs03@gmail.com>
2026-04-06 16:41:38 -0500

    barebones template for profile

--- front-end/src/components/profile.jsx
+++ front-end/src/components/profile.jsx
@@ -3,7 +3,41 @@ import { useState } from "react";
 function Profile() {
   const [view, setView] = useState("overview");
 
-  return <div className="profileDIV"></div>;
+  return (
+    <div className="profileDIV">
+      {/* div below will stetch vh vw */}
+      <div>
+        {/* div below willstretch view heighgt view width for header */}
+        <div className="headerDIV">
+          <div classname="pfpDIV">
+            <img className="pfp" src="" alt="header for.."></img>
+          </div>
+          <div className="whiteSpace"></div>
+        </div>
+        {/* div below will be a position absolute and cover header a bit and also be not fulll vh vw */}
+        <div className="contentsDIV">
+          <div>
+            <img src="" alt="pfp for.."></img>
+          </div>
+          <div>
+            <div></div>
+            <div className="renderViewHere">
+                {view === "overview" &&}
+                {view === "inventory" &&}
+                {view === "inprogress" &&}
+                {view === "limbo" &&}
+                {view === "decluttered" &&}
+                {view === "finished" &&}
+                {view === "likes" &&}
+                {view === "followers" &&}
+                {view === "following" &&}
+                
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
