4a5cb7fd22bd6451c812dab8cbf6fe131cc7ec19
Mena <dayjacs03@gmail.com>
2026-04-06 13:33:10 -0500

    using tanstack

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,26 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+import { Outlet } from "react-router-dom";
+import Welcome from "./components/welcome";
+
+const query = useQuery({
+  queryKey: ["checkSession"],
+  queryFn: sessCheck,
+});
+
+async function sessCheck() {
+  const res = await fetch("http://localHost:5555//session-check-API", {
+    method: "GET",
+    credentials: "include",
+  });
+
+  return res.json();
+}
 
 function App() {
   const [user, setUser] = useState(null);
   const [sessCheckAPIErr, setSessCheckAPIErr] = useState(false);
 
-  useState(() => {
-    async function sessCheck() {
-      try {
-        const res = await fetch("http://localHost:5555//session-check-API", {
-          method: "GET",
-          credentials: "include",
-        });
-      } catch (error) {
-        console.log(error);
-        setSessCheckAPIErr(error);
-        setUser(null);
-      }
-    }
-    sessCheck();
-  }, []);
-
-  return <div className="mainDIV"></div>;
+  return (
+    <div className="mainDIV">
+      {!user && <Welcome setTheUser={setUser} />}
+      {user && (
+        <div className="authUserDiv">
+          <Outlet></Outlet>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,16 +1,25 @@
 import { React } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import App from "./components/index" 
+
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: 
+    element: <App />,
+    children: [
+      {path: , element: }
+    ]
   }
 ]);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
