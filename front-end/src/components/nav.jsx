aaf6687329880316d675097604ef18184acfc196
Mena <dayjacs03@gmail.com>
2026-04-07 11:21:05 -0500

    working on nav

--- front-end/src/components/nav.jsx
+++ front-end/src/components/nav.jsx
@@ -1,5 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
-function Nav() {}
+function Nav() {
+  return (
+    <div clasnName="navDIV">
+      <div className="home"></div>
+      <div className="feed"></div>
+      <div className="make-post"></div>
+      <div className="profile"></div>
+      <div className="open-utils"></div>
+    </div>
+  );
+}
 
 export default Nav;
