81ea5495bbcab283778e5c5431acbc694ccd77c9
Mena <dayjacs03@gmail.com>
2026-04-07 11:26:08 -0500

    working on nav

--- front-end/src/components/nav.jsx
+++ front-end/src/components/nav.jsx
@@ -1,14 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 function Nav() {
+  const { utilsOpen, setUtilsOpen } = useState(false);
+
+  function logout() {}
+
   return (
     <div clasnName="navDIV">
       <div className="home"></div>
       <div className="feed"></div>
       <div className="make-post"></div>
       <div className="profile"></div>
-      <div className="open-utils"></div>
+      <div clasName="utils-div">
+        {utilsOpen && (
+          <div className="utilsModelBackground" onClick={setUtilsOpen(false)}>
+            <Link to="/settings" className="go-to-settings">
+              Settings
+            </Link>
+            <div className="logout" onClick={logout}>
+              Logout
+            </div>
+          </div>
+        )}
+        <div
+          className="open-utils"
+          onClick={() => setUtilsOpen((prev) => !prev)}
+        ></div>
+      </div>
     </div>
   );
 }
