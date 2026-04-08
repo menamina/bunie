b1080030926476a21ec6691881dd4f6648069d14
Mena <dayjacs03@gmail.com>
2026-04-08 15:28:52 -0500

    nuking settings jsx bc i want to change it

--- front-end/src/components/settings.jsx
+++ front-end/src/components/settings.jsx
@@ -1,58 +1,9 @@
 import { useState } from "react";
 
 function Settings() {
-  const [settingView, setSettingView] = useState("overview");
-
-  const [imgChange, setIMGChange] = useState({
-    pfp: "",
-    header: "",
-  });
-
-  const [changePassword, setChangePassword] = useState({
-    oldPassword: "",
-    newPassword: "",
-    confirmNewpassword: "",
-  });
-
-  const [deleteAccountClicked, setDeleteAccountClicked] = useState(false);
-  const [confirmedToDeleteAcc, setConfirmedToDeleteAcc] = useState(false);
-
   return (
     <div className="settingsDIV">
-      <div>Settings</div>
-      <div>
-        <div>
-          <div onClick={() => setSettingView("overview")}>Account</div>
-          <div onClick={() => setSettingView("delete")}>Delete account</div>
-        </div>
-        <div>
-          {settingView === "overview" && (
-            <div>
-              <div className="" onClick={() => setSettingView("change info")}>
-                Account information
-              </div>
-              <div
-                className=""
-                onClick={() => setSettingView("change password")}
-              >
-                Change your password
-              </div>
-            </div>
-          )}
-          {settingView === "change info" ||
-            (settingView === "change password" && (
-              <div>
-                <div className="" onClick={() => setSettingView("overview")}>
-                  go back button
-                </div>
-                <div className="">
-                  {settingView === "change info" && <div></div>}
-                  {settingView === "change password" && <div></div>}
-                </div>
-              </div>
-            ))}
-        </div>
-      </div>
+      <div>Edit Profile</div>
     </div>
   );
 }
