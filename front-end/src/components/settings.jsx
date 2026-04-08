0ef290b7a834f40191c54bff08bd0a673c660fe5
Mena <dayjacs03@gmail.com>
2026-04-08 15:42:11 -0500

    settings

--- front-end/src/components/settings.jsx
+++ front-end/src/components/settings.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 function Settings() {
+  const { user } = useOutletContext();
+  const [settingsView, setSettingsView] = useState("overview");
+
   return (
     <div className="settingsDIV">
-      <div>Edit Profile</div>
+      <div>
+        <div onChange={() => setSettingsView("overview")}>Overview</div>
+        <div onChange={() => setSettingsView("change password")}>
+          Change Password
+        </div>
+        <div onChange={() => setSettingsView("delete")}>Nuke Account</div>
+      </div>
     </div>
   );
 }
