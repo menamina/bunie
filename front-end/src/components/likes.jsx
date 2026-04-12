1fafdeb9f0f132e6a53c472967634b99be3826fd
Mena <dayjacs03@gmail.com>
2026-04-12 18:53:29 -0500

    working on rendering user likes components

--- front-end/src/components/likes.jsx
+++ front-end/src/components/likes.jsx
@@ -1,4 +1,7 @@
-import PostCard from "./postcard";
+import { useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCommentOpt, toggleCommentLikeOpt } from "./ts-queries/queries";
 
 function Likes({ whoseProfile }) {}
 
