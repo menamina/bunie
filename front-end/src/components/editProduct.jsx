648bc2b878eaf8958f9a522eca0a9dbd91abc154
Mena <dayjacs03@gmail.com>
2026-04-14 17:41:58 -0500

    editing product jsx

--- front-end/src/components/editProduct.jsx
+++ front-end/src/components/editProduct.jsx
@@ -3,5 +3,40 @@ import { updateProductMut } from "../ts-queries/queries";
 import { useOutletContext } from "react-router-dom";
 import { useState } from "react";
 
-function EditProduct({ product, closeEdit, closeProdOpts }) {}
+function EditProduct({ product, closeEdit, closeProdOpts }) {
+
+    const [inventoryINFO, setInventoryINFO] = useState({
+    brand: product.brand,
+    product: product.product,
+    category: product.category,
+    price: product.price,
+    img: product.img,
+    status: product.status,
+    backup: product.backup,
+    purchaseDate: product.purchaseDate,
+    rating: product.rating,
+    notes: product.notes,
+    wouldBuyAgain: product.wouldBuyAgain,
+  });
+
+    const queryClient = useQueryClient()
+
+    const {
+        mutate: updateProduct,
+        error: updateErr,
+        isPending: updatePending,
+        reset: resetUpdate
+    } = useMutation({
+        ...updateProductMut();
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["view-status", ] })
+        }
+
+    })
+
+    return (
+
+    )
+
+}
 export default EditProduct;
