2ebf7b8631f61f23c2a8a031c413bd0284282ee6
Mena <dayjacs03@gmail.com>
2026-04-24 19:12:21 -0500

    working on profile jsx

--- front-end/src/components/profile.jsx
+++ front-end/src/components/profile.jsx
@@ -12,6 +12,8 @@ import Likes from "./likes";
 import Follow from "./follow";
 import SelectedView from "./selectedView";
 
+import "../css/profile.css";
+
 function Profile() {
   const { username } = useParams();
   const { user } = useOutletContext();
@@ -84,85 +86,88 @@ function Profile() {
         <div className="whiteSpace"></div>
       </div>
       {/* div below will be a position absolute and cover header a bit and also be not fulll vh vw */}
-      <div className="userINFO">
-        <div>
-          <img
-            src={userProfile?.profile?.pfp || ""}
-            alt={`pfp for ${userProfile?.username}`}
-          ></img>
-        </div>
-        <div>
-          <div>
-            <div className="">
-              <div>{userProfile?.name}</div>
-              <div>@{userProfile?.username}</div>
-            </div>
-            <div>
-              <div>{userProfile?.profile?.bio}</div>
-            </div>
-            <div>
-              <div className="following" onClick={() => setView("following")}>
-                {userProfile?.following?.length || 0} following
-              </div>
-              <div className="followers" onClick={() => setView("followers")}>
-                {userProfile?.followers?.length || 0} followers
-              </div>
-            </div>
-          </div>
 
+      <div className="z-index user content">
+        <div className="userINFO">
           <div>
-            {/* follow / unfollow */}
-            {user?.username === username && (
-              <Link to="/settings">Edit Profile</Link>
-            )}
-            {user.username !== username && (
+            <img
+              src={userProfile?.profile?.pfp || ""}
+              alt={`pfp for ${userProfile?.username}`}
+            ></img>
+          </div>
+          <div>
+            <div>
+              <div className="">
+                <div>{userProfile?.name}</div>
+                <div>@{userProfile?.username}</div>
+              </div>
               <div>
-                {userProfile.followers.has(user.id) ? (
-                  <div onClick={() => unfollow(userProfile.id)}>Unfollow</div>
-                ) : (
-                  <div onClick={() => follow(userProfile.id)}>Follow</div>
-                )}
+                <div>{userProfile?.profile?.bio}</div>
               </div>
-            )}
+              <div>
+                <div className="following" onClick={() => setView("following")}>
+                  {userProfile?.following?.length || 0} following
+                </div>
+                <div className="followers" onClick={() => setView("followers")}>
+                  {userProfile?.followers?.length || 0} followers
+                </div>
+              </div>
+            </div>
+
+            <div>
+              {/* follow / unfollow */}
+              {user?.username === username && (
+                <Link to="/settings">Edit Profile</Link>
+              )}
+              {user.username !== username && (
+                <div>
+                  {userProfile.followers.has(user.id) ? (
+                    <div onClick={() => unfollow(userProfile.id)}>Unfollow</div>
+                  ) : (
+                    <div onClick={() => follow(userProfile.id)}>Follow</div>
+                  )}
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="opts-rendering">
-        <div onClick={() => setView("overview")}>Overview</div>
-        <div onClick={() => setView("inventory")}>Inventory</div>
-        <div onClick={() => setView("inprogress")}>In Progress</div>
-        <div onClick={() => setView("limbo")}>Limbo</div>
-        <div onClick={() => setView("decluttered")}>Decluttered</div>
-        <div onClick={() => setView("finished")}>Finished</div>
-        <div onClick={() => setView("likes")}>Likes</div>
-      </div>
+        <div className="opts-rendering">
+          <div onClick={() => setView("overview")}>Overview</div>
+          <div onClick={() => setView("inventory")}>Inventory</div>
+          <div onClick={() => setView("inprogress")}>In Progress</div>
+          <div onClick={() => setView("limbo")}>Limbo</div>
+          <div onClick={() => setView("decluttered")}>Decluttered</div>
+          <div onClick={() => setView("finished")}>Finished</div>
+          <div onClick={() => setView("likes")}>Likes</div>
+        </div>
 
-      {/* render comps below w fetch to diff api */}
-      <div className="renderViewHere">
-        {view === "overview" && <Overview whoseProfile={username} />}
-        {view === "inventory" && (
-          <SelectedView view="inventory" whoseProfile={username} />
-        )}
-        {view === "inprogress" && (
-          <SelectedView view="inprogress" whoseProfile={username} />
-        )}
-        {view === "limbo" && (
-          <SelectedView view="limbo" whoseProfile={username} />
-        )}
-        {view === "decluttered" && (
-          <SelectedView view="decluttered" whoseProfile={username} />
-        )}
-        {view === "finished" && (
-          <SelectedView view="finished" whoseProfile={username} />
-        )}
-        {view === "likes" && <Likes whoseProfile={username} />}
-        {view === "followers" && (
-          <Follow whoseProfile={username} view="followers" />
-        )}
-        {view === "following" && (
-          <Follow whoseProfile={username} view="following" />
-        )}
+        {/* render comps below w fetch to diff api */}
+        <div className="renderViewHere">
+          {view === "overview" && <Overview whoseProfile={username} />}
+          {view === "inventory" && (
+            <SelectedView view="inventory" whoseProfile={username} />
+          )}
+          {view === "inprogress" && (
+            <SelectedView view="inprogress" whoseProfile={username} />
+          )}
+          {view === "limbo" && (
+            <SelectedView view="limbo" whoseProfile={username} />
+          )}
+          {view === "decluttered" && (
+            <SelectedView view="decluttered" whoseProfile={username} />
+          )}
+          {view === "finished" && (
+            <SelectedView view="finished" whoseProfile={username} />
+          )}
+          {view === "likes" && <Likes whoseProfile={username} />}
+          {view === "followers" && (
+            <Follow whoseProfile={username} view="followers" />
+          )}
+          {view === "following" && (
+            <Follow whoseProfile={username} view="following" />
+          )}
+        </div>
       </div>
     </div>
   );
