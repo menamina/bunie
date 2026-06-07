737da0c7d78e0f55566a7197912baa0fdead4cba
mena <dayjacs03@gmail.com>
2026-06-07 16:09:37 -0500

    validators

--- server/utils/__tests__/zod.test.js
+++ server/utils/__tests__/zod.test.js
@@ -8,9 +8,27 @@ import {
   updateProfZod,
 } from "../zod.js";
 
-it("validates post title and or body strings", () => {
-  const body = {
-    title: "testing",
-    body: "",
+function mockRes() {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+}
+
+it("validates makeOrUpdatePostZod with title and no body", () => {
+  const req = {
+    body: {
+      title: "testing",
+    },
   };
+
+  const res = mockRes();
+
+  const next = jest.fn();
+
+  makeOrUpdatePostZod(req, res, next);
+
+  expect(next).toHaveBeenCalled();
+  expect(res.status).not.toHaveBeenCalled();
+  expect(res.json).not.toHaveBeenCalled();
 });
