aa7260153d90c9e784ae2d32a272ffe08209df3a
mena <dayjacs03@gmail.com>
2026-06-07 15:41:17 -0500

    testing utils

--- server/utils/__tests__/zod.test.js
+++ server/utils/__tests__/zod.test.js
@@ -1,1 +1,16 @@
 // Tests for zod schemas
+
+import {
+  searchZod,
+  makeOrUpdatePostZod,
+  makeOrUpdateCommentZod,
+  addOrUpdateInventoryZod,
+  updateProfZod,
+} from "../zod.js";
+
+it("validates post title and or body strings", () => {
+  const body = {
+    title: "testing",
+    body: "",
+  };
+});
