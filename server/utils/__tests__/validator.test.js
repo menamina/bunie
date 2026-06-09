3c0ba76497e004440970706e35e731b36976110a
mena <dayjacs03@gmail.com>
2026-06-09 15:07:31 -0500

    unit testing validator

--- server/utils/__tests__/validator.test.js
+++ server/utils/__tests__/validator.test.js
@@ -1,1 +1,24 @@
-// Tests for validator utility
+import validation from "../validator";
+
+function mocRes() {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+}
+
+it("validates full signup data that is correct", () => {
+  const req = {
+    body: {
+      name: "tester",
+      username: "tester123",
+      email: "tester@example.com",
+      password: "password123",
+      confirmPassword: "password123",
+    },
+  };
+
+  const res = mocRes();
+
+  const next = jest.fn();
+});
