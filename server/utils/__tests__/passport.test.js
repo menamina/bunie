416ea620588c5ceeada84cf6666f0d5854983443
mena <dayjacs03@gmail.com>
2026-06-09 15:17:45 -0500

    adding passport testing

--- server/utils/__tests__/passport.test.js
+++ server/utils/__tests__/passport.test.js
@@ -1,1 +1,10 @@
-// Tests for passport configuration
+import passport from "../passport";
+
+function mockRes() {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+}
+
+it("finds valid email with password in database", async () => {});
