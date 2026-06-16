80c21c62865d347a951b518f66fff04ada83fa87
Mena <dayjacs03@gmail.com>
2026-06-16 17:31:20 -0500

    working on posts, likes, comments testing

--- server/router/__tests__/postsCommentsLikes.test.js
+++ server/router/__tests__/postsCommentsLikes.test.js
@@ -19,13 +19,17 @@ it("does not get comment by invalid id", async () => {});
 
 it("it likes a post with a valid id", async () => {});
 
+it("unlikes a post");
+
 it("it does not like a post with a invalid id", async () => {});
 
 it("likes a comment with valid id", async () => {});
 
+it("unlikes a post");
+
 it("does not like a comment with invalid id", async () => {});
 
-// posts
+// post||patch posts + comments
 
 it("makes a post with required data that is valid - no images ", async () => {});
 
@@ -34,3 +38,20 @@ it("makes a post with required data that is valid - with 3 images ", async () =>
 it("does not make a post with more than 4 images even if required data is valid", async () => {});
 
 it("does not make a post with missing required data", async () => {});
+
+it("makes a comment with valid post id", async () => {});
+
+it("does not make a comment with inalid post id", async () => {});
+
+it("updates a post with valid id and valid required data", async () => {});
+
+it("does not update a post with valid id and invalid required data", async () => {});
+
+it("updates a comment with valid id and valid required data", async () => {});
+
+it("does not update a comment with valid id and invalid required data", async () => {});
+
+// deleting posts + comments
+
+it("deletes a post");
+it("deletes a comment");
