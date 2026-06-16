81f97a9780794f7eea128e90df7831c1de7bbd26
Mena <dayjacs03@gmail.com>
2026-06-15 19:29:30 -0500

    setting up skeletons for testing

--- server/router/__tests__/feedAndSettings.test.js
+++ server/router/__tests__/feedAndSettings.test.js
@@ -1,7 +1,25 @@
 const app = require("../../server");
 const supertest = require("supertest");
-request = supertest(app);
+const request = supertest(app);
+const agent = supertest.agent(app);
 const prisma = require("../../prisma/client");
+
+async function createTestUser(
+  email = "test@gmail.com",
+  username = "test",
+  password = "hello",
+) {
+  const hashedPassword = await passwordGenie(password);
+  return await prisma.user.create({
+    data: {
+      name: "Test User",
+      username,
+      email,
+      saltedHash: hashedPassword,
+      profile: { create: { bio: null } },
+    },
+  });
+}
 
 afterAll(async () => {
   await prisma.$disconnect();
@@ -10,14 +28,18 @@ afterAll(async () => {
 // main feed
 it("does not return images multer does not have", async () => {});
 
-it("gets main feed posts when authenticated", async () => {});
+it("gets main feed posts when authenticated", async () => {
+  const res = await agent.get("/main-feed-API").send();
+  expect(res.status).toBe(200);
+  expect(res.body).toHaveProperty("feed");
+});
 
 it("gets main feed posts when not authenticated", async () => {});
 
 // following feed
 it("gets following feed posts when authenticated", async () => {});
 
-it("gets following feed posts when not authenticated", async () => {});
+it("does not get following feed posts when not authenticated", async () => {});
 
 // getting settings
 it("gets logged in users profile settings when authenticated and correct user", async () => {});
@@ -39,5 +61,7 @@ it("updates profile with valid info", async () => {});
 it("does not update profile with non valid info", async () => {});
 it("updates password when current password matches database", async () => {});
 it("does not update profile when current password does not match database", async () => {});
-it("updates password when current pass === database AND new pass meets validation", async () => {});
-it("does not update password when current pass === database AND new pass does not meet validation", async () => {});
+it("updates password when current pass === database AND new pass meets validation AND confirmed password is matches new password", async () => {});
+it("does not update password when current pass === database AND new pass does not match confirmed password", async () => {});
+it("does not delete user account when who is logged in does not match account to be deleted", async () => {});
+it("deletes user account when who is logged in does match account to be deleted", async () => {});
