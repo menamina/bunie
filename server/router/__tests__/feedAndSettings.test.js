ba2f7e375c9018c1108b2c2a6a40c8ce94afc751
Mena <dayjacs03@gmail.com>
2026-06-15 19:06:51 -0500

    setting up skeletons for testing

--- server/router/__tests__/feedAndSettings.test.js
+++ server/router/__tests__/feedAndSettings.test.js
@@ -6,3 +6,20 @@ const prisma = require("../../prisma/client");
 afterAll(async () => {
   await prisma.$disconnect();
 });
+
+// main feed
+it("gets main feed posts when authenticated", async () => {});
+
+it("gets main feed posts when not authenticated", async () => {});
+
+// following feed
+it("gets following feed posts when authenticated", async () => {});
+
+it("gets following feed posts when not authenticated", async () => {});
+
+//
+it("gets logged in users profile settings when authenticated and correct user", async () => {});
+
+it("does not get a users settings when not logged in", async () => {});
+
+it("does not get a users settings when logged in but NOT correct user", async () => {});
