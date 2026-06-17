9a71efa022c6d352317b36f92f34cab71abc522b
Mena <dayjacs03@gmail.com>
2026-06-17 15:03:06 -0500

    tests for profiles _ inventory

--- server/router/__tests__/profsAndInventory.test.js
+++ server/router/__tests__/profsAndInventory.test.js
@@ -1,19 +1,84 @@
 const app = require("../../server");
 const supertest = require("supertest");
-request = supertest(app);
+const request = supertest(app);
+const agent = supertest.agent(app);
 const prisma = require("../../prisma/client");
+const { passwordGenie } = require("../../utils/password");
+
+// helper functions
+async function createTestUser(
+  email = "test@gmail.com",
+  username = "test",
+  password = "12345678",
+) {
+  const hashedPassword = await passwordGenie(password);
+  return await prisma.user.create({
+    data: {
+      name: "Test User",
+      username,
+      email,
+      saltedHash: hashedPassword,
+      profile: { create: { bio: null } },
+    },
+  });
+}
+
+const user = createTestUser();
+
+const agent = supertest.agent(app);
+
+async function login() {
+  return await agent.post("/login-API").send({
+    email: "test@gmail.com",
+    password: "12345678",
+  });
+}
+
+async function logout() {
+  return await agent.post("/log-out");
+}
+
+async function dlt(userID) {
+  return await prisma.user.delete({ where: { id: userID } });
+}
 
 afterAll(async () => {
+  dlt();
   await prisma.$disconnect();
 });
 
-it("gets logged in users profile");
+it("gets logged in users profile", async () => {
+  login();
+  const res = await agent.get("/profile-API/test");
+  expect(res.status).toBe(200);
+  expect(res.body.username).toEqual("test");
+  logout();
+});
 
-it("gets users profile with an account that is active", async () => {});
+it("gets users profile with an account that is active", async () => {
+  const newUser = createTestUser("faker@gmail.com", "notTest");
+  login();
+  const res = await agent.get(`/profile-API/${newUser.username}`);
+  expect(res.status).toBe(200);
+  expect(res.body.username).toEqual(newUser.username);
+  dlt(newUser.id);
+  logout();
+});
 
-it("does not users profile with an account that is deleted", async () => {});
+it("does not get users profile with an account that is deleted or nonexistant", async () => {
+  login();
+  const res = await agent.get("/profile-API/notTest");
+  expect(res.status).toBe(404);
+  expect(res.body).toHaveProperty("noUserFound");
+  logout();
+});
 
-it("gets users follower count by username", async () => {});
+it("gets users follower count by username", async () => {
+  login();
+  const res = await agent.get(`get-user-followers/${user.username}`);
+  expect(res.status).toBe(404);
+  expect(res.body).toHaveProperty("message");
+});
 
 it("gets users following count by username", async () => {});
 
