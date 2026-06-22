18d1be60b470304d762fd9bb3a3878ea8555000a
Mena <dayjacs03@gmail.com>
2026-06-22 10:15:10 -0500

    describing tests

--- server/router/__tests__/followSearch.test.js
+++ server/router/__tests__/followSearch.test.js
@@ -66,38 +66,42 @@ afterAll(async () => {
   await prisma.$disconnect();
 });
 
-it("follows another user", async () => {
-  const res = await agent.post(`/follow/${otherUser.id}`);
-  expect(res.status).toBe(201);
-  expect(res.body).toHaveProperty("following");
-  expect(res.body.following).toBe(true);
+describe("follows", () => {
+  it("follows another user", async () => {
+    const res = await agent.post(`/follow/${otherUser.id}`);
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty("following");
+    expect(res.body.following).toBe(true);
+  });
+
+  it("does not follow self", async () => {
+    const res = await agent.post(`/follow/${user.id}`);
+    expect(res.status).toBe(400);
+    expect(res.body.success).toBe(false);
+  });
+
+  it("it unfollows another user", async () => {
+    await agent.post(`/follow/${otherUser.id}`);
+
+    const res = await agent.post(`/follow/${otherUser.id}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("following");
+    expect(res.body.following).toBe(false);
+  });
 });
 
-it("does not follow self", async () => {
-  const res = await agent.post(`/follow/${user.id}`);
-  expect(res.status).toBe(400);
-  expect(res.body.success).toBe(false);
-});
+describe("query", () => {
+  it("searches query with at least one character", async () => {
+    const res = await agent.get("/search-API?querySearch=test&tabView=top");
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("usersWithQuery");
+    expect(res.body).toHaveProperty("postsWithQuery");
+    expect(res.body).toHaveProperty("nextCursor");
+  });
 
-it("it unfollows another user", async () => {
-  await agent.post(`/follow/${otherUser.id}`);
-
-  const res = await agent.post(`/follow/${otherUser.id}`);
-  expect(res.status).toBe(200);
-  expect(res.body).toHaveProperty("following");
-  expect(res.body.following).toBe(false);
-});
-
-it("searches query with at least one character", async () => {
-  const res = await agent.get("/search-API?querySearch=test&tabView=top");
-  expect(res.status).toBe(200);
-  expect(res.body).toHaveProperty("usersWithQuery");
-  expect(res.body).toHaveProperty("postsWithQuery");
-  expect(res.body).toHaveProperty("nextCursor");
-});
-
-it("does not search query with 0 characters", async () => {
-  const res = await agent.get("/search-API?querySearch=");
-  expect(res.status).toBe(400);
-  expect(res.body).toHaveProperty("error");
+  it("does not search query with 0 characters", async () => {
+    const res = await agent.get("/search-API?querySearch=");
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty("error");
+  });
 });
